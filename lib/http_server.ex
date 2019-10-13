cd1698e5e7b0542640317809259189dd15016f89
Daniel <daniel.carneiro@rentbrella.com>
2019-10-13 12:52:24 -0300

    Done: add supervisor and tasks configuration | Where: lib/http_server.ex | Context: start the server process

--- lib/http_server.ex
+++ lib/http_server.ex
@@ -2,11 +2,19 @@ defmodule HTTPSERVER do
   require Logger
   use Application
 
-  def start(_type, {opts, [file | _]}) do
+  def start(_type, {opts, [file|_]}) do
     import Supervisor.Spec, warn: false
 
-    Logger.info("starting server...")
-    Logger.info("server started")
+    port = Keyword.get(opts, :port) || Application.get_env(:http_server, :port)
+    app = HTTPSERVER.CodeLoader.load(file)
 
+    children = [
+      {Task.Supervisor, name: HTTPSERVER.Server.Supervisor},
+      {HTTPSERVER.Listener, port: port, app: app}
+    ]
+
+    opts = [strategy: :one_for_all, name: HTTPSERVER.Supervisor]
+
+    Supervisor.start_link(children, opts)
   end
 end
