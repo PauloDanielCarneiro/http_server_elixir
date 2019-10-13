f999050832e21611fc108ab9fecbff8d7ea72383
pegueraeu <peguera_eu@live.com>
2019-10-13 18:19:04 -0300

    Done: added new endpoints and made different return messages and statuses for existing ones | Where: app/myapp.exs | Context: making more endpoints to show different http responses

--- app/myapp.exs
+++ app/myapp.exs
@@ -1,13 +1,32 @@
 defmodule MyApp do
   # handle the / path
   def call(%{method: "GET", path: "/"}),
-    do: %{code: 200, type: "text/plain", body: "GET"}
+    do: %{code: 200, type: "text/plain", body: "Returns a List of resources"}
 
-  def call(%{method: "POST", path: "/"}),
-    do: %{code: 200, type: "text/plain", body: "POST"}
+  def call(%{method: "POST", path: "/create"}),
+    do: %{
+      code: 201,
+      type: "application/json",
+      body:
+        "{\"message\":\"This method creates a new resource and returns 201\",\"status\":\"created\"}"
+    }
+
+  def call(%{method: "GET", path: "/amiauthorized"}),
+    do: %{
+      code: 401,
+      type: "application/json",
+      body: "{\"message\":\"You\"re not authorized\",\"status\":\"unauthoried\"}"
+    }
+
+  def call(%{method: "GET", path: "/teapot"}),
+    do: %{
+      code: 418,
+      type: "application/json",
+      body: "{\"message\":\"Use me\",\"status\":\"i'm a teapot\"}"
+    }
 
   def call(%{method: "DELETE", path: "/"}),
-    do: %{code: 200, type: "text/plain", body: "DELETE"}
+    do: %{code: 204, type: "text/plain", body: ""}
 
   def call(%{method: "OPTIONS", path: "/"}),
     do: %{code: 200, type: "text/plain", body: "HEAD, GET, POST, DELETE, OPTIONS"}
@@ -15,7 +34,18 @@ defmodule MyApp do
   def call(%{method: "HEAD", path: "/"}),
     do: %{code: 200, type: "text/plain", body: ""}
 
+  def call(%{method: "POST", path: "/blazeit"}),
+    do: %{
+      code: 420,
+      type: "text/plain",
+      body: "Chill, bro \n Listen to this: https://www.youtube.com/watch?v=mACqcZZwG0k"
+    }
+
   # everything else is a 404 response
   def call(_),
-    do: %{code: 404, body: "say what now?"}
+    do: %{
+      code: 404,
+      type: "text/plain",
+      body: "This is not the response you're looking for, move along"
+    }
 end
